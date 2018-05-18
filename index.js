5e63d6b1603e59f2721bb87275d07552623e8902
krishnan2098 <krishnan2098@gmail.com>
2018-05-18 21:01:04 +0530

    added heading

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var dataset = [1,2,3,4,5];
 
 d3.select('h1').style('color', 'red')
     .attr('class','heading')
-    .text('Heading!');
+    .text('First Heading!');
 
 d3.select('body')
     .append('p')
