25724b88c4503897f43d18337cf51a90912a1197
krishnan2098 <krishnan2098@gmail.com>
2018-05-18 21:24:30 +0530

    Implemented Scaling for dataset having smaller range of values compared to the height of svg container

--- index4.js
+++ index4.js
@@ -1,25 +1,30 @@
-//Creating labels for the bar chart
+//Creating labels, Scales for the bar chart
 
 //Creating simple bar chart
-var dataset = [ 80, 100, 56, 120, 180, 30, 40, 120, 160];
+// var dataset = [ 80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var dataset = [1,2,3,4,5];// for small data the chart isnt scaled. hence scaling is required.
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = svgWidth/dataset.length;
 var barWidth = svgWidth/dataset.length;
 
 var svg = d3.select('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
 
+var yScale = d3.scaleLinear()
+    .domain([0,d3.max(dataset)])
+    .range([0,svgHeight]);
+
 var barChart = svg.selectAll('rect') //bar graphs are simply rectangles
     .data(dataset)
     .enter()
     .append('rect')
     .attr('y', function(d) {
-        return svgHeight - d;
+        return svgHeight - yScale(d);
     })
     .attr('height', function(d) {
-        return d;
+        return yScale(d);
     })
     .attr('width', barWidth - barPadding)
     .attr('transform', function(d, i){
@@ -35,7 +40,7 @@ var labelText = svg.selectAll('text') //bar graphs are simply rectangles
         return d;
     })
     .attr('y', function(d) {
-        return svgHeight - d - 2;
+        return svgHeight - yScale(d) + 15;
     })
     .attr('x', function(d, i){
         return barWidth * i;
